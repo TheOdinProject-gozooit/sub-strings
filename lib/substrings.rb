f36ea8ff04318c51c5913d1f3834509a3765947d
gozooit <gozoo.it@protonmail.com>
2023-04-12 13:02:20 +0200

    remove puts in method

--- lib/substrings.rb
+++ lib/substrings.rb
@@ -9,18 +9,19 @@ def substrings(str, dictionnary)
 
     res[word] = matches.length
   end
-  # ! We are using puts here to comply with the exercise, but would be better to return the value instead,
-  # ! then use "puts substrings(str, dictionary)" to see the output
-  puts res
   res
 end
 
 # Add TOP example so can be run with "ruby ./substrings.rb"
 
-dictionary = ['below','down','go','going','horn','how','howdy','it','i','low','own','part','partner','sit']
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-substrings('below', dictionary) # return { "below" => 1, "low" => 1 }
-
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+puts "Dictionary is : #{dictionary}"
+puts
+puts 'Results for string "below" :'
+puts substrings('below', dictionary) # return { "below" => 1, "low" => 1 }
+puts
+puts "Results for string \"Howdy partner, sit down! How's it going?\" :"
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
 # return { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1,
 #          "part" => 1, "partner" => 1, "sit" => 1 }
