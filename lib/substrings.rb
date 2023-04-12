1000151e2c5b5bcc2e093cd8a60476b1ddbb600a
gozooit <gozoo.it@protonmail.com>
2023-04-12 12:36:29 +0200

    make it case insensitive with downcase

--- lib/substrings.rb
+++ lib/substrings.rb
@@ -2,7 +2,8 @@ def substrings(str, dictionnary)
   res = {}
   splitted_str = str.split(' ')
   dictionnary.each do |word|
-    matches = splitted_str.select { |split| split.include?(word) }
+    word.downcase!
+    matches = splitted_str.select { |split| split.downcase.include?(word) }
     next if matches.empty?
 
     res[word] = matches.length
